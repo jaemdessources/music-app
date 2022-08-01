480186a44559b089ea1048a6ae5db14e30f49b90
Jaem <pianodessource@gmail.com>
2022-08-01 00:10:55 -0400

    "Added a rotation animation to the cover image"

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,10 +1,14 @@
 import React from "react";
 // adding components
 
-function Song({ currentSong }) {
+function Song({ currentSong, isPlaying }) {
   return (
     <div className="song-container">
-      <img alt={currentSong.name} src={currentSong.cover}></img>
+      <img
+        className={isPlaying ? "rotating" : ""}
+        alt={currentSong.name}
+        src={currentSong.cover}
+      ></img>
       <h2>{currentSong.name}</h2>
       <h3>{currentSong.artist}</h3>
     </div>
